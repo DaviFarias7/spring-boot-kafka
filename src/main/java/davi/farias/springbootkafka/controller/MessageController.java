a69ab27496bb7c499423095a1bc7ae90cda4c72b
davi.farias <davi.farias@unimedjp.com.br>
2024-03-26 13:10:55 -0300

    Create class KafkaConsumer and MessaageController

--- src/main/java/davi/farias/springbootkafka/controller/MessageController.java
+++ src/main/java/davi/farias/springbootkafka/controller/MessageController.java
@@ -1,2 +1,24 @@
-package davi.farias.springbootkafka.controller;public class MessageController {
+package davi.farias.springbootkafka.controller;
+
+import davi.farias.springbootkafka.kafka.KafkaProducer;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/kafka")
+public class MessageController {
+
+    private KafkaProducer kafkaProducer;
+
+    public MessageController(KafkaProducer kafkaProducer) {
+        this.kafkaProducer = kafkaProducer;
+    }
+    @GetMapping("/publish")
+    public ResponseEntity<String> publish(@RequestParam("message") String message){
+        kafkaProducer.sendMessage(message);
+        return ResponseEntity.ok("Message sent to the topic");
+    }
 }
--- src/main/java/davi/farias/springbootkafka/kafka/KafkaConsumer.java
+++ src/main/java/davi/farias/springbootkafka/kafka/KafkaConsumer.java
@@ -1,2 +1,17 @@
-package davi.farias.springbootkafka.kafka;public class KafkaConsumer {
+package davi.farias.springbootkafka.kafka;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaProducer.class);
+
+    @KafkaListener(topics = "javaguides", groupId = "myGroup")
+    public void consume(String message){
+        LOGGER.info(String.format("Message receiver -> %s", message));
+    }
 }
